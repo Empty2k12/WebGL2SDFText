fabcea55c6f2d516b273f0ae0c2ab5522b60c4b7
Gero Gerke <11deutron11@gmail.com>
2019-01-24 16:14:18 +0100

    Fix Babel setup for deployment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import '@babel/register';
+import '@babel/polyfill';
+
 import webgl2sdftext from './webgl2sdftext';
 
 webgl2sdftext();
--- webpack.config.js
+++ webpack.config.js
@@ -5,52 +5,52 @@ const APP_DIR = path.resolve(__dirname, 'src');
 const BUILD_DIR = path.resolve(__dirname, 'public');
 
 const config = {
-    entry: `${APP_DIR}/index.js`,
-    output: {
-        path: BUILD_DIR,
-        filename: 'assets/js/bundle.js',
+  entry: `${APP_DIR}/index.js`,
+  output: {
+    path: BUILD_DIR,
+    filename: 'assets/js/bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+    modules: [APP_DIR, 'node_modules'],
+    alias: {
+      constants: `${APP_DIR}/constants`,
     },
-    resolve: {
-        extensions: ['.js', '.json'],
-        modules: [APP_DIR, 'node_modules'],
-        alias: {
-            constants: `${APP_DIR}/constants`,
-        },
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: APP_DIR,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader'],
-            },
-            {
-                test: [/\.vert$/, /\.frag$/, /\.glsl$/],
-                use: ['raw-loader'],
-            },
-        ],
-    },
-    devServer: {
-        contentBase: BUILD_DIR,
-        port: 8080,
-    },
-    plugins: [
-        new CopyWebpackPlugin([
-            { from: 'src/font/', to: `${BUILD_DIR}/font/` },
-        ]),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: APP_DIR,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: [/\.vert$/, /\.frag$/, /\.glsl$/],
+        use: ['raw-loader'],
+      },
     ],
+  },
+  devServer: {
+    contentBase: BUILD_DIR,
+    port: 8080,
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'src/font/', to: `${BUILD_DIR}/font/` },
+    ]),
+  ],
 };
 
 module.exports = config;
